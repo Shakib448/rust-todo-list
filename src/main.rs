ba903c1eda1dd557574fa1d42e8fa74677bbdf81
Shakib448 <shakiba448@gmail.com>
2024-09-24 21:21:25 +0600

    WIP on cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+use std::{collections::HashMap, io::Read};
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+fn parse_str_to_bool(s: &str) -> Result<bool, String> {
+    match s.to_lowercase().as_str() {
+        "true" => Ok(true),
+        "false" => Ok(false),
+        _ => Err(format!("Cannot parse '{}' as bool", s)),
+    }
+}
+
+impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+
+        let mut map = HashMap::new();
+
+        for entries in content.lines() {
+            let mut values = entries.split('\t');
+            let key = values.next().expect("No Key");
+            let val = values.next().expect("No Value");
+            let parsed_val = match val {
+                "true" => true,
+                "false" => false,
+                _ => panic!("Invalid boolean value in db.txt: {}", val),
+            };
+
+            map.insert(String::from(key), parsed_val);
+        }
+        Ok(Todo { map })
+    }
+
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record);
+        }
+
+        std::fs::write("db.txt", content)
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("Ad error occurred: {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
+        }
+    }
 }
